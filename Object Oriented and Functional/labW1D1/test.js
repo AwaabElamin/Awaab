2a8b096f67d07904598c0fde7a8d664d8bdb2e76
Awaab Elamin <AwaabElamin@gmail.com>
2021-03-22 14:10:50 -0500

    update test,js

--- Object Oriented and Functional/labW1D1/test.js
+++ Object Oriented and Functional/labW1D1/test.js
@@ -30,26 +30,30 @@ function mochaTests() {
 
     describe("computeGrade", function () {
         it("Compute letter grade", function () {
-            assert.equal(computeGrade([90,90,90]), "A");
+            assert.equal(computeGrade([90, 90, 90]), "A");
         });
     });
     describe("computeGrade", function () {
         it("Compute letter grade", function () {
-            assert.equal(computeGrade([80,80,80]), "B");
+            assert.equal(computeGrade([80, 80, 80]), "B");
         });
     });
     describe("computeGrade", function () {
         it("Compute letter grade", function () {
-            assert.equal(computeGrade([70,70,70]), "C");
+            assert.equal(computeGrade([70, 70, 70]), "C");
         });
     });
     describe("computeGrade", function () {
         it("Compute letter grade", function () {
-            assert.equal(computeGrade([60,60,60]), "D");
+            assert.equal(computeGrade([60, 60, 60]), "D");
         });
-    });          
+    });
     describe("computeGrade", function () {
         it("Compute letter grade", function () {
-            assert.equal(computeGrade([59,59,59]), "NC");
+            assert.equal(computeGrade([59, 59, 59]), "NC");
         });
-    });        }
+        it("Compute letter grade", function () {
+            assert.equal(computeGrade([]), "NC");
+        });
+    });
+}
